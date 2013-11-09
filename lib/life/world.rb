38a8c67cd64e67e581d786263f06fd5365a54715
Thomas Lundström <thomas.lundstrom@gmail.com>
2013-11-09 21:06:14 +0100

    1st stab on implementing world

--- lib/life/world.rb
+++ lib/life/world.rb
@@ -1,5 +1,11 @@
 module Life
   class World
 
+    attr_reader :state
+
+    def initialize(state)
+      @state = state
+    end
+
   end
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -6,4 +6,31 @@ describe Life::World do
     Life::World.class.to_s.should == "Class"
   end
 
+
+  context "blinker" do
+    let(:world) {
+      Life::World.new([
+        [0,0,0],
+        [1,1,1],
+        [0,0,0]])
+    }
+
+    it "has state" do
+      world.state.should == [
+        [0,0,0],
+        [1,1,1],
+        [0,0,0]
+      ]
+    end
+
+    it "blinks" do
+      pending "todo"
+      world.tick.state.should == [
+        [0,1,0],
+        [0,1,0],
+        [0,1,0],
+      ]
+    end
+  end
+
 end
